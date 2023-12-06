eeed9ae9961fd7bdad72935f7e38b16a32cbd219
Karl Mårtensson <karlmartenssonsales@gmail.com>
2023-12-06 13:33:04 +0100

    initial commit

---
+++ Connection-DB/Program.cs
@@ -0,0 +1,32 @@
+﻿using Connection_DB;
+using Npgsql;
+
+
+string db = "Host=localhost;Port=5455;Username=postgres;Password=postgres;Database=myDB";
+
+await using var myDB = NpgsqlDataSource.Create(db);
+
+await using (var table = myDB.CreateCommand("CREATE TABLE IF NOT EXISTS Users (ID SERIAL PRIMARY KEY, FullName VARCHAR(100), Username VARCHAR(20) UNIQUE, Password VARCHAR(20), Email varchar(100) UNIQUE, DoB Date);"))
+{
+    await table.ExecuteNonQueryAsync();
+}
+
+Users user = new Users();
+
+user.RegisterUser();
+
+await using (var users = myDB.CreateCommand("INSERT INTO Users (FullName, Username, Password, Email, DoB) VALUES (@Fullname, @Username, @Password, @Email, @DoB) "))
+{
+    users.Parameters.AddWithValue("@Fullname", user.fullname);
+    users.Parameters.AddWithValue("@Username", user.username);
+    users.Parameters.AddWithValue("@Password", user.password);
+    users.Parameters.AddWithValue("@Email", user.email);
+    users.Parameters.AddWithValue("@DoB", DateTime.Parse(user.DoB));
+    await users.ExecuteNonQueryAsync();
+}
+
+
+
+
+
+
---
+++ Connection-DB/Users.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Connection_DB
+{
+    public class Users
+    {
+        public string fullname;
+        public string username;
+        public string password;
+        public string email;
+        public string DoB;
+
+        public void RegisterUser()
+        {
+            Console.Write("Full Name: ");
+            fullname = Console.ReadLine();
+            Console.Clear();
+
+            Console.Write("Username: ");
+            username = Console.ReadLine();
+            Console.Clear();
+
+            Console.Write("Password: ");
+            password = Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("Email: ");
+            email = Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("Date of Birth (Like this ->[xxxx-xx-xx]");
+            DoB = Console.ReadLine();
+            Console.Clear();
+        }
+    }
+}
